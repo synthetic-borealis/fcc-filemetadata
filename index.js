f82af8e995e5c324949b36d47c7461efdceae0fd
synthetic-borealis <snip.coloured@gmail.com>
2022-12-26 16:25:04 +0200

    iteration 1

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: './uploads' });
 require('dotenv').config()
+const fs = require('fs');
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +14,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse/', upload.single('upfile'), function (req, res, next) {
+  const { file } = req;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  });
+  fs.unlinkSync(file.path);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
